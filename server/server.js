27caa598326b67b330ef1326e169e0b8c2f0a086
Don Choppy <wxtem@hotmail.com>
2018-07-20 02:55:50 -0400

    se crean routes (controladores) y se agrega conexion a mongo

--- server/server.js
+++ server/server.js
@@ -3,47 +3,20 @@ require('./config/config');
 
 // require modules
 const express = require('express');
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // declare app express
 const app = express();
 
-// use body-parser to parse everything
-app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
+app.use(require('./routes/usuario'));
 
 
-// services
-app.get('/usuario', (request, response) => {
-    response.json('get Usuario');
+// mongo connect
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+    console.log("db online");
 });
 
-app.post('/usuario', (request, response) => {
-    let body = request.body;
-
-    if (body.nombre === undefined) {
-        response.status(400).json({
-            ok: false,
-            msg: "Nombre es necesario"
-        });
-    } else {
-
-        response.json({
-            persona: body
-        });
-    }
-    let id = request.params.id;
-});
-
-app.put('/usuario/:id', (request, response) => {
-    response.json({
-        id
-    });
-});
-
-app.delete('/usuario', (request, response) => {
-    response.json('delete Usuario');
-});
 
 // listen
 app.listen(process.env.PORT, () => {
